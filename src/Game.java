f0d5686bafd971265fc4148303625f4ec177f589
peppa-pig <lchen1074@gmail.com>
2026-05-13 17:02:36 +0800

    add new

---
+++ src/Game.java
@@ -0,0 +1,18 @@
+
+
+public class Game extends GameEngine{
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void update(double dt) {
+
+    }
+
+    @Override
+    public void paintComponent() {
+
+    }
+
+}
